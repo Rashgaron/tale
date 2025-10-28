5a45aee9191a1e6721af6868ee53510dd16562f8
Rashgaron <dannirodriguez99@gmail.com>
2025-10-28 23:59:57 +0100

    fix build

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import styles from "./home.module.css";
-import OpenAI from "openai";
 
-export const Home = async () => {
+export const Home = () => {
   const { homeContainer, title, cardElement, listCardContainer } = styles;
-  const [selectedTopic, setSelectedTopic] = useState<string>();
+  const [selectedTopic, setSelectedTopic] = useState<string>("");
   console.log("Selected Topic: ", selectedTopic);
   const topics: string[] = [
     "cats",
@@ -37,24 +36,16 @@ export const Home = async () => {
     );
   };
 
-  const requestTale = () => {
 
-  }
 
-const client = new OpenAI();
-
-const response = await client.responses.create({
-    model: "gpt-5",
-    input: "Write a one-sentence bedtime story about a unicorn."
-});
-
-console.log(response.output_text);
   return (
     <div className={homeContainer}>
       <h1 className={title}>Daily Tale</h1>
       <p>Please, choose the topic of the tale that you want</p>
       <ListCards elements={topics} />
-      <button disabled={!selectedTopic} onClick={requestTale}>Validate</button>
+      { 
+        selectedTopic && <p>{selectedTopic}</p>
+      }
     </div>
   );
 };
